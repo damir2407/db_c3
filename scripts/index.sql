5e5faa18720a81d8d89af3d0cb9e066e3b582834
Дамир <70958074+damir2407@users.noreply.github.com>
2022-12-18 23:35:41 +0300

    Delete scripts directory

--- scripts/index.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX Инвентарь_index ON "Инвентарь" USING btree("Логин_Пользователя", "Внутриигровая_вещь ID");
-
-CREATE UNIQUE INDEX Библиотека_index ON "Библиотека" USING btree("Логин_Пользователя", "Игра_ID");
-
-CREATE INDEX Кошелек_index ON "Кошелек" USING hash("Логин_Пользователя");
-
-CREATE INDEX Игра_index ON "Игра" USING hash ("Название");
-
-CREATE INDEX Магазин_index ON "Магазин" USING hash("Игра_ID");
